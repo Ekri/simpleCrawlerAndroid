3146927923f89d70ebb08d88b5a14e86b4adb62a
Bartosz Maciega <b.maciega@futuremind.com>
2016-10-25 12:42:52 +0200

    simple search in data

--- app/src/main/java/com/maciega/bartosz/crawl/DataActivity.java
+++ app/src/main/java/com/maciega/bartosz/crawl/DataActivity.java
@@ -1,10 +1,15 @@
 package com.maciega.bartosz.crawl;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v4.view.MenuItemCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.SearchView;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
 import android.view.MenuItem;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.ListView;
 
 import com.maciega.bartosz.crawl.adapter.DataAdapter;
@@ -17,11 +22,12 @@ import java.util.List;
  * Created by bartoszmaciega on 17/10/16.
  */
 
-public class DataActivity extends AppCompatActivity implements DataPresenter.DataView {
+public class DataActivity extends AppCompatActivity implements DataPresenter.DataView, SearchView.OnQueryTextListener {
 
     ListView listView;
     DataAdapter dataAdapter;
     private DataPresenter presenter;
+    private SearchView searchView;
 
 
     @Override
@@ -51,8 +57,20 @@ public class DataActivity extends AppCompatActivity implements DataPresenter.Dat
 
 
     @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.search_menu, menu);
+        MenuItem searchItem = menu.findItem(R.id.search);
+        SearchView searchView = (SearchView) MenuItemCompat.getActionView(searchItem);
+        searchView.setOnQueryTextListener(this);
+        this.searchView = searchView;
+        MenuItemCompat.setOnActionExpandListener(searchItem, searchExpandListener);
+
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        if(item.getItemId() == android.R.id.home){
+        if (item.getItemId() == android.R.id.home) {
             super.onBackPressed();
         }
         return super.onOptionsItemSelected(item);
@@ -63,8 +81,36 @@ public class DataActivity extends AppCompatActivity implements DataPresenter.Dat
     }
 
 
+    private MenuItemCompat.OnActionExpandListener searchExpandListener = new MenuItemCompat.OnActionExpandListener() {
+        @Override
+        public boolean onMenuItemActionExpand(MenuItem item) {
+            return true;
+        }
+
+        @Override
+        public boolean onMenuItemActionCollapse(MenuItem item) {
+            presenter.finishQuery();
+            return true;
+        }
+    };
+
+
     @Override
     public void showUrls(List<Url> urls) {
         dataAdapter.setData(urls);
+    }
+
+    @Override
+    public boolean onQueryTextSubmit(String query) {
+        presenter.submitQuery(query);
+        InputMethodManager imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+        imm.hideSoftInputFromWindow(searchView.getWindowToken(), 0);
+        return true;
+    }
+
+    @Override
+    public boolean onQueryTextChange(String newText) {
+
+        return false;
     }
 }
--- app/src/main/java/com/maciega/bartosz/crawl/presenter/DataPresenter.java
+++ app/src/main/java/com/maciega/bartosz/crawl/presenter/DataPresenter.java
@@ -7,8 +7,11 @@ import com.maciega.bartosz.crawl.model.Url;
 import com.maciega.bartosz.crawl.storage.DbStorage;
 import com.maciega.bartosz.crawl.storage.Storage;
 
+import java.util.Collections;
 import java.util.List;
 
+import io.realm.Realm;
+import io.realm.RealmResults;
 import rx.Observable;
 import rx.Subscriber;
 import rx.android.schedulers.AndroidSchedulers;
@@ -51,7 +54,7 @@ public class DataPresenter {
                 .subscribe(new Action1<List<Url>>() {
                                @Override
                                public void call(List<Url> urls) {
-                                    dataView.showUrls(urls);
+                                   dataView.showUrls(urls);
                                }
                            },
                         new Action1<Throwable>() {
@@ -62,6 +65,46 @@ public class DataPresenter {
                         });
 
 
+    }
+
+
+    public void submitQuery(final String query) {
+        Observable.create(new Observable.OnSubscribe<List<Url>>() {
+            @Override
+            public void call(Subscriber<? super List<Url>> subscriber) {
+                subscriber.onNext(proceedQuery(query));
+            }
+        }).subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(new Action1<List<Url>>() {
+                    @Override
+                    public void call(List<Url> urls) {
+                        dataView.showUrls(urls);
+                    }
+                }, new Action1<Throwable>() {
+                    @Override
+                    public void call(Throwable throwable) {
+                        Log.d("error", throwable.getLocalizedMessage());
+                    }
+                });
+
+
+    }
+
+    public void finishQuery() {
+        loadData();
+    }
+
+
+    /**
+     * temporary fast written method,
+     * rethink with database model in future
+     */
+    private List<Url> proceedQuery(String query) {
+        Realm realm = Realm.getDefaultInstance();
+        RealmResults<Url> results = realm.where(Url.class)
+                .contains("url", query).findAll();
+        return realm.copyFromRealm(results);
     }
 
 
